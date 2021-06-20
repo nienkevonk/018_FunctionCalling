d417c41777452dd49fea42e3c79905104c081046
nienkevonk <84348574+nienkevonk@users.noreply.github.com>
2021-06-20 13:38:44 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 // hey kiddo
 
-// const age = function (number) {
-//     if (number >= 18) {
-//         return "true";
-//     }
-// };
+const age = function (number) {
+    if (number >= 18) {
+        return "true";
+    }
+};
 
-// const adultcheck = function (number) {
-//     if (age(number)) {
-//         return "Hello there";
-//     } return "Hey kiddo";
-// };
+const adultcheck = function (number) {
+    if (age(number)) {
+        return "Hello there";
+    } return "Hey kiddo";
+};
 
-// console.log(adultcheck(20));
-// console.log(adultcheck(17));
+console.log(adultcheck(20));
+console.log(adultcheck(17));
 
 // VAT calculations exercise 1
 
-// const vatcalculation = function (baseprice, VATpercentage) {
-//     const price = baseprice * (VATpercentage / 100);
-//     return price;
-// };
+const vatcalculation = function (baseprice, VATpercentage) {
+    const price = baseprice * (VATpercentage / 100);
+    return price;
+};
 
-// const priceincludeVAT = function (baseprice, VATpercentage) {
-//     const VAT = vatcalculation(baseprice, VATpercentage)
-//     return baseprice + VAT;
-// };
+const priceincludeVAT = function (baseprice, VATpercentage) {
+    const VAT = vatcalculation(baseprice, VATpercentage)
+    return baseprice + VAT;
+};
 
-// console.log(priceincludeVAT(1000, 21));
+console.log(priceincludeVAT(1000, 21));
 
 // // VAT calculations excercise 2
 
